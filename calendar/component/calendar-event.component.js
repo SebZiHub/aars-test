da01ecc013eeae6ec97b532a2f86ffdf65e0700a
lufonius <fonjallaz97@gmail.com>
2024-07-14 19:58:06 +0200

    remove bootstrap, darkmode styling of calendar

--- calendar/component/calendar-event.component.js
+++ calendar/component/calendar-event.component.js
@@ -3,32 +3,32 @@ export default {
   methods: {
     getClassForDayBadge: (event) => {
       if (event.isNextEventComingUp()) {
-        return "badge badge-primary";
+        return "badge badge-big badge-primary";
       } else {
-        return "badge badge-secondary";
+        return "badge badge-big badge-secondary";
       }
     }
   },
 
   mounted() {
-    // console.log(this.event);
   },
   props: ['event'],
   template: `
-  <div class="row" v-if="event.isFutureEvent()">
-    <div class="col-2">
-      <h1><span :class="getClassForDayBadge(this.event)"><span>{{ event.getDay() }}</span></span></h1>
-      <p><span>{{ event.getMonthAbbreviation() }}</span></p>
+  <i data-feather="circle"></i>
+  <div class="event-container" v-if="event.isFutureEvent()">
+    <div class="date-container">
+      <span :class="getClassForDayBadge(this.event)"><span>{{ event.getDay() }}</span></span>
+      <span class="month-abbr">{{ event.getMonthAbbreviation() }}</span>
     </div>
-    <div class="col-10">
-      <h3><strong>Cube in {{ event.location.city.name }}</strong></h3>
-      <ul class="list-inline">
-            <li class="list-inline-item"><i class="fa fa-calendar-o" aria-hidden="true"></i>{{ event.getDateInSwissFormat() }}</li>
-        <li class="list-inline-item"><i class="fa fa-clock-o" aria-hidden="true"></i>{{ event.getStartTime() }} - {{ event.getEndTime() }}</li>
-        <span>
-          <li class='list-inline-item'><i class='fa fa-location-arrow' aria-hidden='true'></i><a target='_blank' :href="event.location.googleMapsLink">{{ event.location.name }}</a></li>
-        </span>
-      </ul>
+    <div class="main-container">
+      <div class="title-container">
+        <span><strong>Cube in {{ event.location.city.name }}</strong></span>
+      </div>
+      <div class="info-container">
+        <div><i class="fa-solid fa-calendar" aria-hidden="true"></i><span>{{ event.getDateInSwissFormat() }}</span></div>
+        <div><i class="fa-solid fa-clock" aria-hidden="true"></i><span>{{ event.getStartTime() }} - {{ event.getEndTime() }}</span></div>
+        <div><i class='fa-solid fa-location-dot' aria-hidden='true'></i><span><a target='_blank' :href="event.location.googleMapsLink">{{ event.location.name }}</a></span></div>
+      </div>
     </div>
   </div>
   `
